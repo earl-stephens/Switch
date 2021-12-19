e8168014c71a0c85400e21aa339f542c62d8e6b5
earl-stephens <stephensearl@hotmail.com>
2021-12-19 15:37:12 -0700

    Set up a switch statement

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,24 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int option = 10;
+		// Typically use an int for the variable
+		switch (option) {
+		// Does the option have the value 0
+		case 0:
+			System.out.println("Option 0 selected.");
+			break;
+		case 1:
+			System.out.println("Option 1 selected.");
+			break;
+		case 10:
+			System.out.println("Option 10 selected. Exiting");
+			System.exit(0);
+			break;
+		// The below isn't required, but is a good idea
+		default:
+			System.out.println("Invalid option.");
+			break;
+		}
 	}
-
 }
